2b2b5cc513a5c6347dc67d05f6f380db65e7c764
Caleb Stultz <kalub92@gmail.com>
2016-03-05 06:25:13 +0700

    Basic UI set up and Core Data entities created.

---
+++ Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Recipes
+//
+//  Created by Caleb Stultz on 3/5/16.
+//  Copyright © 2016 Caleb Stultz. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Recipe: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
