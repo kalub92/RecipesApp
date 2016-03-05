266a3c1031ce953b23fa1e729c8c1ad870f02273
Caleb Stultz <kalub92@gmail.com>
2016-03-05 07:25:05 +0700

    Main UI Working, still need to add data storing functions for adding new recipe

---
+++ Recipies/RecipeCell.swift
@@ -0,0 +1,26 @@
+//
+//  RecipeCell.swift
+//  Recipes
+//
+//  Created by Caleb Stultz on 3/5/16.
+//  Copyright © 2016 Caleb Stultz. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UITableViewCell {
+    
+    @IBOutlet weak var recipeTitle: UILabel!
+    @IBOutlet weak var recipeImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell(recipe: Recipe) {
+        recipeTitle.text = recipe.title
+        recipeImg.image = recipe.getRecipeImg()
+    }
+
+}
